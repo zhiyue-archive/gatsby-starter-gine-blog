635ec3eb69d56ffaca0e8ba7606efade78ec5198
zhiyue <cszhiyue@gmail.com>
2019-12-16 12:07:41 +0800

    update database

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    plugins: [
-        {
+    plugins: [{
             resolve: `gatsby-theme-gine-blog`,
             options: {
                 configTable: "https://www.notion.so/zhiyue/68db99a329d1498aaf1e5ff7af519f7e?v=90354b40c8e843b9ae201bb27b98b54f"
             }
         },
         {
-            resolve: `gatsby-source-notion-database`,
+            resolve: `zhiyue-gatsby-source-notion-database`,
             options: {
                 configTable: "https://www.notion.so/zhiyue/4299f917a182451bac7d4f8e970818e5?v=8181d285bf54472dbcb6dd427ef00942"
             }
